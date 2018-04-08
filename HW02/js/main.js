c79e33ceb3b45a2dbe33d631ebbeaad31d8dbb62
joonas <joonas92@gmail.com>
2018-04-08 14:46:59 +0300

    Life class change, basic methods
    made life class dependent on data value of 'lifeCount' and added methods for losing and gaining lives.

--- HW02/js/main.js
+++ HW02/js/main.js
@@ -5,10 +5,26 @@
 
 
 const appState = {
-
+    lifeCount: 3,
+    score: 0
 };
 var vm = new Vue({
     data: appState,
-    methods: {}
+    methods: {
+        loseLife: function (event) {
+            this.lifeCount -= 1;
+            if (this.lifeCount == 0) {
+                alert("Game Over");
+            }
+        },
+        gainLife: function (event) {
+            if (this.lifeCount < 3) {
+                this.lifeCount += 1;
+            }
+        },
+        doubleScore: function (event) {
+            this.score *= 2;
+        }
+    }
 
 }).$mount('#game');
